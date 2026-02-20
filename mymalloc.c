43c902d2a7c081613154a1b3c5c7b5f9d0f13195
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2026-02-20 13:21:43 -0500

    added my_free()

--- mymalloc.c
+++ mymalloc.c
@@ -4,20 +4,22 @@ static union {
     char bytes[MEMLENGTH];
     double not_used;
 } heap;
-static int intialized = 0;
+static int initialized = 0;
 typedef struct{
     int size;
     int allocated;
 } chunk_header;
 static size_t round8(size_t sz) {
-    return (sz + 7) & ~(size_t)7; 
+    return (sz + 7) & ~(size_t)7;
 }
+static chunk_header *next_chunk(chunk_header *h);
+static int inside_heap(void *ptr);
 static void leak_detector(){
     chunk_header *chunk = heap.bytes;
     int objects = 0;
     int size = 0;
     while(1){
-        if(chunk->allocated=1){
+        if(chunk->allocated==1){
             objects++;
             size+=chunk->size;
         }
@@ -33,21 +35,21 @@ static void leak_detector(){
 void initialize_heap(){
     chunk_header init = {MEMLENGTH-sizeof(chunk_header),0};
     *(chunk_header *)heap.bytes = init;
-    intialized = 1;
+    initialized = 1;
     atexit(leak_detector);
 }
 static chunk_header *next_chunk(chunk_header *h){
     return (chunk_header *)((sizeof(chunk_header)+h->size) + (char *)h);
 }
-static int inside_heap(chunk_header *h){
-    if((char *)h > heap.bytes && (char *)h<heap.bytes+MEMLENGTH){
-        return 1;
-    }else{
-        return 0;
+static int inside_heap(void *ptr){
+    if ((char *)ptr >= heap.bytes && (char *)ptr < heap.bytes + MEMLENGTH) {
+      return 1;
+    } else {
+      return 0;
     }
 }
 void *mymalloc(size_t size, char *file, int line){
-    if(!intialized) initialize_heap();
+    if(!initialized) initialize_heap();
     if(size == 0) return NULL;
     size = round8(size);
     chunk_header *chunk = heap.bytes;
@@ -75,5 +77,29 @@ void *mymalloc(size_t size, char *file, int line){
     }
 }
 void   myfree (void *ptr, char *file, int line){
+  if(inside_heap(ptr)) {
+    fprintf(stderr,"free: Innapropriate pointersodoes (%s:%d)\n", file, line);
+    exit(2);
+  }
+  chunk_header *chunk = (chunk_header*)heap.bytes;
+  while(inside_heap(chunk)) {
+    void *payload = (void*)(chunk + 1);
+    if(payload == ptr) {
+      if(chunk->allocated == 0) {
+        fprintf(stderr,"free: Innapropriate pointer (%s:%d)\n", file, line);
+        exit(2);
+      }
+      chunk->allocated = 0;
+      while(1) {
+        chunk_header *next = next_chunk(chunk);
+        if (!inside_heap(next) || next ->allocated == 1) { break; }
+        chunk->size += sizeof(chunk_header) + next->size;
+      }
+      return;
+    }
+    chunk = next_chunk(chunk);
+  }
 
+  fprintf(stderr,"free: Innapropriate pointer (%s:%d)\n", file, line);
+  exit(2);
 }
